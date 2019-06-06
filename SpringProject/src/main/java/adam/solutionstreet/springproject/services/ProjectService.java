9b393364a4fb5e665f7001d7baeded7e9517dbc9
unknown <aboudion@solutionstreet.com>
2019-06-06 18:14:42 -0400

    progress commit June 6th

--- SpringProject/src/main/java/adam/solutionstreet/springproject/services/ProjectService.java
+++ SpringProject/src/main/java/adam/solutionstreet/springproject/services/ProjectService.java
@@ -1,6 +1,7 @@
 package adam.solutionstreet.springproject.services;
 
 import adam.solutionstreet.springproject.domain.Project;
+import adam.solutionstreet.springproject.exceptions.ProjectIdException;
 import adam.solutionstreet.springproject.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,7 +13,36 @@ public class ProjectService {
     private ProjectRepository projectRepository;
 
     public Project saveOrUpdateProject(Project project){
+        try {
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        } catch (Exception ex){
+            throw new ProjectIdException("Project ID '" + project.getProjectIdentifier().toUpperCase() + "' already exists");
+        }
+    }
 
-        return projectRepository.save(project);
+    public Project findProjectByIdentifer(String projectId){
+
+        Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+
+        if (project == null){
+            throw new ProjectIdException("Project ID '" + projectId + "' doesn't exist");
+        }
+
+        return project;
+    }
+
+    public Iterable<Project> findAllProjects(){
+        return projectRepository.findAll();
+    }
+
+    public void deleteProjectByIdentifier(String projectId){
+        Project project = projectRepository.findByProjectIdentifier(projectId);
+
+        if(project == null){
+            throw new ProjectIdException("Cannot delete Project with ID '" + projectId + "'. This project does not exist");
+        }
+
+        projectRepository.delete(project);
     }
 }
